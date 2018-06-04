beeb45db3bd7d7f3ed81c277808b6d5476377d0a
Thibaut DUFOUR <thibaut.dufour@outlook.com>
2018-06-04 09:02:41 +0200

    Logout

--- src/java/servlets/LogoutEmployee.java
+++ src/java/servlets/LogoutEmployee.java
@@ -14,30 +14,12 @@ import javax.servlet.http.HttpSession;
 public class LogoutEmployee extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
 
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Employee Logout</title>");
-		out.println("<link rel='stylesheet' href='resources/bootstrap.min.css'/>");
-		out.println("<link rel='stylesheet' href='style.css'/>");
-		out.println("</head>");
-		out.println("<body>");
-		request.getRequestDispatcher("navhome.html").include(request, response);
-		out.println("<div class='container'>");
-
-		out.println("<h1>Admin Logout Success</h1>");
 		HttpSession session = request.getSession();
 		session.invalidate();
-		request.getRequestDispatcher("EmployeeLoginForm.html").include(request, response);
 
-		out.println("</div>");
-		request.getRequestDispatcher("footer.html").include(request, response);
-		out.println("</body>");
-		out.println("</html>");
+		request.getRequestDispatcher("index.html").include(request, response);
 
-		out.close();
 	}
 
 }
